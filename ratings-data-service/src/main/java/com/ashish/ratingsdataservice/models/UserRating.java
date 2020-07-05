6a860b8f0be8e279bf5a327cc4f3497ef60c0557
Ashishkumar Gupta <akumar9@walmartlabs.com>
2020-07-05 22:35:37 +0530

    10. Calling external movie API, don't foget to add the TMDB token before calling the API

--- ratings-data-service/src/main/java/com/ashish/ratingsdataservice/models/UserRating.java
+++ ratings-data-service/src/main/java/com/ashish/ratingsdataservice/models/UserRating.java
@@ -27,8 +27,8 @@ public class UserRating {
     public void initData(String userId) {
         this.setUserId(userId);
         this.setRatings(Arrays.asList(
-                new Rating("1234", 5),
-                new Rating("5678", 9)
+                new Rating("100", 3),
+                new Rating("200", 4)
         ));
     }
 }
